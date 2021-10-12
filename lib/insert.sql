983017c023517919712e99aeab37abfdeec6c420
MichaelPisciotta <mfp56@miami.edu>
2021-10-12 16:04:27 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,10 @@
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Mr. Chocolate", "1", "M", "red", "relaxed", "true")
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Rowdy", "1", "M", "red", "relaxed", "true")
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Tabitha", "1", "M", "red", "relaxed", "true")
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Sergeant Brown", "1", "M", "red", "relaxed", "true")
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Melissa", "1", "M", "red", "relaxed", "true")
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Grinch", "1", "M", "red", "relaxed", "true")
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Wendy", "1", "M", "red", "relaxed", "true")
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Mr. Chocolate", "1", "M", "red", "relaxed", "true"),
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Rowdy", "1", "M", "red", "relaxed", "true"),
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Tabitha", "1", "M", "red", "relaxed", "true"),
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Sergeant Brown", "1", "M", "red", "relaxed", "true"),
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Melissa", "1", "M", "red", "relaxed", "true"),
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Grinch", "1", "M", "red", "relaxed", "true"),
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "Wendy", "1", "M", "red", "relaxed", "true"),
 INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES("1", "", "1", "M", "red", "relaxed", "true")
 
 
